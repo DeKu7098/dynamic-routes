a8501e5abdc541708ccfc53f1c1ba0b741423ab6
Arka <arkac720@gmail.com>
2023-03-30 13:14:52 +0530

    Full stack Booking Appointment App

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 
-const errorController = require('./controllers/error');
+// const errorController = require('./controllers/error');
+
+const User = require('./models/User');
+var cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 // db.execute('SELECT * FROM products').then((result) => {
 //     console.log(result[0], result[1]);
@@ -20,19 +26,61 @@ const shopRoutes = require('./routes/shop');
 //     console.log(err);
 // });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
+
+app.post('/user/add-user', async (req, res, next) => {
+    try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const phonenumber = req.body.phonenumber;
+
+    const data = await User.create( {name: name, email: email, phonenumber: phonenumber});
+    res.status(201).json({newUserDetail: data});
+    }catch(err){
+       res.status(500).json({
+        err: err
+       })
+    }
+})
+
+app.get('/user/get-users', async(req, res, next) => {
+    try {
+    const users = await User.findAll();
+    res.status(200).json({allUsers: users});
+    }catch(err){
+        console.log("Get users is failing", JSON.stringify(err));
+        res.status(500).json({
+            err:err
+        })
+    }
+})
+
+app.delete('/user/delete-user/:id', async (req, res, next) => {
+    try{
+        if(req.params.id == 'undefined'){
+            console.log('Id is missing');
+            return res.status(400).json({err: 'ID is missing'})
+        }
+    const uId = req.params.id;
+    await User.destroy({where: {id: uId}});
+    res.sendStatus(200);
+    }catch(err){
+        console.log(err);
+      res.status(500).json(err);
+    }
+})
 
 sequelize
 .sync()
 .then(result => {
     // console.log(result);
-    app.listen(3000);
+    app.listen(3001);
 })
 .catch(err => {
     console.log(err);
